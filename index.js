95de71616a12b517fdaecb53aee9041598f3a521
Tova Yocheved Hillman <hillman.t.y@gmail.com>
2024-04-18 13:09:01 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+   customerName = customerName.toUpperCase();
+};
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+setBestCustomer(bestCustomer);
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+overwriteBestCustomer(bestCustomer);
+
+
+
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = "Tim";
+    leastFavoriteCustomer = "David"
+}
+
+changeLeastFavoriteCustomer()
